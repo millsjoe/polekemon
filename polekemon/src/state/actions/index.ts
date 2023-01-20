cbeb47a3a5d6156cc4b3ea7e2f150e7048f63d10
Joe mills <Joe.mills@skybettingandgaming.com>
2023-01-20 10:37:39 +0000

    added actions

--- polekemon/src/state/actions/index.ts
+++ polekemon/src/state/actions/index.ts
@@ -1,3 +1,22 @@
 // actions go here
 
-export {}
+export enum ActionKeys {
+    INCREMENT = "INCREMENT",
+    DECREMENT = "DECREMENT",
+}
+
+export const Actions = {
+    increment: (amount: number) => {
+        return {
+            type: ActionKeys.INCREMENT,
+            payload: amount,
+        };
+    },
+
+    decrement: (amount: number) => {
+        return {
+            type: ActionKeys.DECREMENT,
+            payload: amount,
+        };
+    }
+};
